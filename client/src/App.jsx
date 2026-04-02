66e5070eb7cf74bf550f670d3e07ba7e7c5f2680
Sirichandanakota <kotasirichandana7@gmail.com>
2026-04-02 21:42:41 +0530

    Implement multi-page navigation in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import TemplatesPage from './pages/TemplatesPage';
 import ResumeEditor from './pages/ResumeEditor';
 
 export default function App() {
-  return (
-    <div className="min-h-screen">
-      <ResumeEditor />
-    </div>
-  );
+  const [currentPage, setCurrentPage] = useState('editor'); // direct editor
+  const [template, setTemplate] = useState(null);
+
+  // LOGIN
+  if (currentPage === 'login') {
+    return (
+      <LoginPage
+        onLogin={() => setCurrentPage('templates')}
+        onSwitchToSignUp={() => setCurrentPage('signup')}
+      />
+    );
+  }
+
+  // SIGNUP
+  if (currentPage === 'signup') {
+    return (
+      <SignUpPage
+        onSignUp={() => setCurrentPage('templates')}
+        onSwitchToLogin={() => setCurrentPage('login')}
+      />
+    );
+  }
+
+  // TEMPLATES
+  if (currentPage === 'templates') {
+    return (
+      <TemplatesPage
+        onSelect={(tmpl) => {
+          setTemplate(tmpl);
+          setCurrentPage('editor');
+        }}
+      />
+    );
+  }
+
+  // RESUME EDITOR (MAIN UI)
+  if (currentPage === 'editor') {
+    return (
+      <ResumeEditor
+        template={template}
+        onBack={() => setCurrentPage('templates')}
+      />
+    );
+  }
+
+  return null;
 }
