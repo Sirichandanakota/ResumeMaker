01b1cc74177e78b43eac4c561f87e272c9ed57c3
Sirichandanakota <kotasirichandana7@gmail.com>
2026-04-02 20:21:14 +0530

    Remove import for HomePage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import TemplatesPage from './pages/TemplatesPage';
