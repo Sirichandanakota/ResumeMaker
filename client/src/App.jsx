4ed37b9abdd7c3e5a70d97bde0a3ac9c02ce4e66
Sirichandanakota <kotasirichandana7@gmail.com>
2026-04-02 21:27:50 +0530

    Remove Footer component from App
    Removed Footer component and adjusted App structure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ResumeEditor from './pages/ResumeEditor';
-import Footer from './components/Footer';
+
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+   
 
       {/* MAIN EDITOR */}
       <div className="flex-grow">
         <ResumeEditor />
       </div>
 
-      {/* FOOTER */}
-      <Footer />
-
-    </div>
   );
 }
